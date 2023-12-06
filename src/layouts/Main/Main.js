6b624e515a40c5beb40b9539e266cef2219ae491
Abhi <abhippateltx@gmail.com>
2023-12-06 17:39:09 +0530

    Don't Know

--- src/layouts/Main/Main.js
+++ src/layouts/Main/Main.js
@@ -6,7 +6,7 @@ import Divider from '@mui/material/Divider';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Slide from '@mui/material/Slide';
-import { Topbar, Sidebar, Footer2 } from './components';
+import { Topbar, Sidebar, Footer2, DontKnow } from './components';
 import Container from 'common/Container';
 import { pages } from '../navigation';
 
@@ -74,6 +74,10 @@ const Main = ({
         {children}
         <Divider />
       </main>
+      <Container paddingY={4}>
+        <DontKnow />
+      </Container>
+      <Divider />
       <Container paddingY={4}>
         <Footer2 />
       </Container>
